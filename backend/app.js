819e1c761c800ec6989a12beb4153b122c5e44f2
trilok <trilokjangid.094@gmail.com>
2024-12-29 17:07:51 +0530

    file

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('./api/v1/reservation', ReservationRouter);
+app.use('/api/v1/reservation', ReservationRouter);
 
 
 app.use(errorMiddleware);
