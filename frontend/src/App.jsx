97f7e315aefad47447550546a642ffa8cba654b2
trilok <trilokjangid.094@gmail.com>
2024-12-29 17:21:02 +0530

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./app.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return <Router>
+    <Routes>
+      <Route path="/" element={ } />
+    </Routes>
+  </Router>
+
+};
+
+export default App
