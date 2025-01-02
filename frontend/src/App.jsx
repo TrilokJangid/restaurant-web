6d9e47f5e4e9a523e84d733ffbe9445317eb3078
trilok <trilokjangid.094@gmail.com>
2025-01-02 17:01:32 +0530

    added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import "./app.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import Home from './pages/Home.js'
+import NotFound from './pages/NotFound.js'
+import Success from './pages/Success.js'
 const App = () => {
   return <Router>
     <Routes>
-      <Route path="/" element={ } />
+      <Route path="/" element={<Home />} />
+      <Route path="/success" element={<Success />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   </Router>
 
